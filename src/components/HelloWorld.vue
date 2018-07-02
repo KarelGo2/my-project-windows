7aabb0e16ed93ea5fdd9dd1551324aae7b3dad3a
Karel <yttcold@gmail.com>
2018-07-03 00:13:21 +0800

    a

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,97 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+   <div id="ytt">
+<div id = "computed_props">
+    千米 : <input type = "text" v-model = "kilometers">
+    米 : <input type = "text" v-model = "meters">
+</div>
+<p id="info"></p>
+<div v-bind:class="{ active: isActive }"></div>
+ <div class="static"
+     v-bind:class="{ active: isActive, 'text-danger': hasError }">
   </div>
+
+  <div v-bind:class="classObject"></div>
+</div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  data : function(){
+    return  {
+      classObject: {
+      active: true,
+      'text-danger': true
+    },
+      hasError: true,
+       isActive: true,
+       kilometers : 0,
+        meters: 0,
+      // seen: true,
+      // object: {
+      // name: '菜鸟教程',
+      // url: 'http://www.runoob.com',
+      // slogan: '学的不仅是技术，更是梦想！'
+      //  },
+      // ok: true,
+      // msg: 'Welcome to Your Vue.js App',
+      message: 'helo',
+      name: 'Google',
+    url: 'http://www.google.com'
+      // ,
+      // type: 'C',
+      // sites: [{ name: 'Runoob' }, { name: 'Google' }, { name: 'Taobao' }]
+   }
+  },
+computed: {
+    site: {
+      // getter
+      get: function () {
+        return this.name + ' ' + this.url
+      },
+      // setter
+      set: function (newValue) {
+        var names = newValue.split(' ')
+        this.name = names[0]
+        this.url = names[names.length - 1]
+      },
+       classObject: function () {
+      return {
+        active: this.isActive && !this.error,
+        'text-danger': this.error && this.error.type === 'fatal',
+      }
     }
-  }
+    }
+  },
+  watch : {
+        kilometers:function(val) {
+            this.kilometers = val;
+            this.meters = val * 1000;
+        },
+        meters : function (val) {
+            this.kilometers = val/ 1000;
+            this.meters = val;
+        }
+    }
 }
+
+// 调用 setter， vm.name 和 vm.url 也会被对应更新
+// vm.site = '菜鸟教程 http://www.runoob.com';
+// document.write('name: ' + vm.name);
+// document.write('<br>');
+// document.write('url: ' + vm.url);
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -109,5 +97,15 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.active {
+	width: 100px;
+	height: 100px;
+	background: green;
+}
+
+.text-danger {
+	background: red;
 }
 </style>
